670a56e26e16d5d1ce8bedeae5fedc1003fd5a5f
abra-abe <abewills17@gmail.com>
2024-09-26 08:40:09 +0300

    postgres database works well...

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = 'postgresql://postgres:swords@localhost:5432/fastAPI-tutorial'
+URL_DATABASE = 'postgresql://postgres:pass@localhost:5432/fastAPI-tutorial'
 
 engine = create_engine(URL_DATABASE)
 
